aee03ec1e14979e6b2c9085a3fd7108d74bfb2a0
morleyj <justmorley@byui.edu>
2022-09-22 10:53:07 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            names.Add("Bob");
+            names.Add("Bill");
+            names.Add("Sue");
+
+            names.ForEach(name => Console.Write(name + " "));
+
+            string aString = "a";
+            List<string> stuff = new List<string> {aString, "b", "c", "d"};
+
+            char aSingleCharacter = 'a';
+            System.Console.WriteLine($"This is a single character: {aSingleCharacter}");
+
+            int x = 5;
+            int y = 7;
+
+            int result = Adder(x,y);
+            System.Console.WriteLine($"{x} + {y} = {result}");
+        }
+
+        static int Adder(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
