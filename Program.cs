4d5a2cdfaadebf7bef7d9080aa0899987bbe312f
morleyj <justmorley@byui.edu>
2022-09-24 02:08:10 -0600

    updated tic tac toe

--- Program.cs
+++ Program.cs
@@ -6,29 +6,52 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            List<string> names = new List<string>();
-            names.Add("Bob");
-            names.Add("Bill");
-            names.Add("Sue");
+            List<string> numbers = new List<string>{"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+            showBoard(numbers);
+            string player = "x";
+            int turn = 1;
+            while (turn < 9)
+            {
+                playerTurn(numbers, player);
+                showBoard(numbers);
+                player = playerChange(player);
+                turn = turn + 1;
+            }
+            
 
-            names.ForEach(name => Console.Write(name + " "));
+            
 
-            string aString = "a";
-            List<string> stuff = new List<string> {aString, "b", "c", "d"};
-
-            char aSingleCharacter = 'a';
-            System.Console.WriteLine($"This is a single character: {aSingleCharacter}");
-
-            int x = 5;
-            int y = 7;
-
-            int result = Adder(x,y);
-            System.Console.WriteLine($"{x} + {y} = {result}");
+        
         }
 
-        static int Adder(int a, int b)
+        static void showBoard(List<string> board)
         {
-            return a + b;
+            /// Prints out the board
+            Console.WriteLine($"{board[0]}|{board[1]}|{board[2]}");
+            Console.WriteLine("-+-+-");
+            Console.WriteLine($"{board[3]}|{board[4]}|{board[5]}");
+            Console.WriteLine("-+-+-");
+            Console.WriteLine($"{board[6]}|{board[7]}|{board[8]}");
+        }
+
+         static void playerTurn(List<string> board, string currentPlayer)
+        {
+            Console.Write($"{currentPlayer}'s turn to choose a square (1-9): ");
+            string move_string = Console.ReadLine();
+            int choice = int.Parse(move_string);
+            int index = choice - 1;
+            board[index] = currentPlayer;
+        }
+
+        static string playerChange(string currentPlayer)
+        {
+            string nextPlayer = "x";
+
+            if(currentPlayer == "x")
+            {
+                nextPlayer = "o";
+            }
+            return nextPlayer;
         }
     }
 }
